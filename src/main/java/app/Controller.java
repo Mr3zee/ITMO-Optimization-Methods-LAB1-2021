2a7c00eebeddac0e82f51f6cf39983be1bc7d3dd
Mr3zee <284627@niuitmo.ru>
2021-03-06 00:02:37 +0300

    toggles done

--- src/main/java/app/Controller.java
+++ src/main/java/app/Controller.java
@@ -1,15 +1,20 @@
 package app;
 
+import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Font;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class Controller implements Initializable {
     @Override
@@ -27,13 +32,13 @@ public class Controller implements Initializable {
         splitPane.setDividerPositions(0.4, 0.6);
     }
 
-    private static Font radioFont;
+    private static Font toggleFont;
 
     private void loadFont() {
         try {
-            radioFont = Font.loadFont(getClass().getResource("/Shrek-Font.ttf").openStream(), 21);
+            toggleFont = Font.loadFont(getClass().getResource("/Shrek-Font.ttf").openStream(), 20);
         } catch (IOException e) {
-            radioFont = Font.getDefault();
+            toggleFont = Font.getDefault();
         }
     }
 
@@ -53,8 +58,8 @@ public class Controller implements Initializable {
     private static final ToggleGroup variantsGroup = new ToggleGroup();
 
     private void setupListViews() {
-        listAlgo.setCellFactory(a -> new RadioListCell(algoGroup));
-        listVariants.setCellFactory(a -> new RadioListCell(variantsGroup));
+        listAlgo.setCellFactory(a -> new ToggleListCell(algoGroup));
+        listVariants.setCellFactory(a -> new ToggleListCell(variantsGroup));
 
         listAlgoNames.addAll(
                 "BRENT", "PARABOLIC", "GOLDEN SECTION", "DICHOTOMY", "FIBONACCI"
@@ -67,13 +72,35 @@ public class Controller implements Initializable {
         listAlgo.setItems(listAlgoNames);
 
         listVariants.setItems(listVariantsNames);
+
+        algoGroup.selectedToggleProperty().addListener(changeToggleImage);
+        variantsGroup.selectedToggleProperty().addListener(changeToggleImage);
     }
 
-    private static class RadioListCell extends ListCell<String> {
-        private final ToggleGroup group;
+    @SuppressWarnings("unchecked")
+    private static final ChangeListener<Toggle> changeToggleImage = (o, oldT, newT) -> {
+        if (oldT != null) {
+            ((Consumer<Boolean>) oldT.getUserData()).accept(false);
+        }
+        if (newT != null) {
+            ((Consumer<Boolean>) newT.getUserData()).accept(true);
+        }
+    };
 
-        private RadioListCell(ToggleGroup group) {
+    private static class ToggleListCell extends ListCell<String> {
+        private final ToggleGroup group;
+        private static final Image selected = new Image("/selected-button.png", 35, 35, true, true);
+        private static final Image notSelected = new Image("/not-selected-button.png", 35, 35, true, true);
+        private final ImageView iv;
+        private final Function<ToggleButton, Consumer<Boolean>> setSelected;
+
+        private ToggleListCell(ToggleGroup group) {
             this.group = group;
+            this.iv = new ImageView(notSelected);
+            setSelected = (button) -> (selected) -> {
+                iv.setImage(selected ? ToggleListCell.selected : notSelected);
+                button.setStyle(String.format("-fx-text-fill: %s", selected ? "#523213" : "#C3BC95"));
+            };
         }
 
         @Override
@@ -83,11 +110,13 @@ public class Controller implements Initializable {
                 setText(null);
                 setGraphic(null);
             } else {
-                RadioButton radioButton = new RadioButton(obj);
-                radioButton.setToggleGroup(group);
-                radioButton.setFont(radioFont);
+                ToggleButton toggleButton = new ToggleButton(obj);
+                toggleButton.setToggleGroup(group);
+                toggleButton.setFont(toggleFont);
+                toggleButton.setGraphic(iv);
+                toggleButton.setUserData(setSelected.apply(toggleButton));
                 // Add Listeners if any
-                setGraphic(radioButton);
+                setGraphic(toggleButton);
             }
         }
     }
